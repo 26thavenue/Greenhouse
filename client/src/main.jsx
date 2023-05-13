39c79358052ad3057ca01ea27ff47e6bd3721308
Oni Oluwatomiwa <oni.oluwatomiwa@stu.cu.edu.ng>
2023-05-13 22:09:46 +0100

    Staretd working..

---
+++ client/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import ErrorPage from './pages/ErrorPage'
+import Login from './pages/Login'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
